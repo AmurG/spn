4195c5fb7279adabe34edd0224c658a5655edffd
amurg <gublaghose@gmail.com>
2017-05-15 04:44:26 -0400

    2

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy
 import networkx as nx
 from scipy.stats import multivariate_normal as mn
+import matplotlib.pyplot as plt
 
 def submat(mat,subset):
 	q = len(subset)
@@ -27,7 +28,7 @@ def submean(mean,subset):
 		w=w+1
 	return m
 
-k = 4
+k = 5
 
 cov = np.zeros(k*k)
 cov = np.reshape(cov,(k,k))
@@ -48,12 +49,16 @@ print(cov)
 
 mean = np.random.uniform(-1,1,k)
 
-gen = np.random.multivariate_normal(mean,cov,100)
+ref = mn(mean=mean,cov=cov)
+
+gen = np.random.multivariate_normal(mean,cov,10000)
 
 #print(np.shape(gen))
 
 estcov = np.corrcoef(np.transpose(gen))
 estcov2 = np.cov(np.transpose(gen))
+
+wts = np.zeros(k)
 
 print(-estcov)
 
@@ -68,6 +73,10 @@ for i in range(0,k-1):
 	Dec.append(list(nx.connected_components(T)))
 	print(np.shape(Order))
 	Order = Order[Order[:,2].argsort()]
+	sum = 0
+	for j in range(0,len(Order)):
+		sum = sum + Order[j,2]
+	wts[i] = sum
 	iter = len(Order)
 	print(Order)
 	idx = int(Order[len(Order)-1,0])
@@ -75,13 +84,63 @@ for i in range(0,k-1):
 	print(idx,idx2)
 	T.remove_edge(idx,idx2)
 
-cset = Dec[1][0]
+Dec.append(list(nx.connected_components(T)))
+PDF = []
+print(Dec)
 
-print(cset)
+for i in range(0,len(Dec)):
+	subpdf = []
+	for j in (Dec[i]):
+		m = submean(mean,j)
+		print(m)
+		c = submat(estcov2,j)
+		print(c)
+		subpdf.append(mn(mean=m,cov=c))
+	PDF.append(subpdf)
+	print(PDF)
 
-print(submat(estcov,cset))
+wts[k-1] = 0.1
 
+run = 0
 
+#wts[0] = 0
+
+for i in range(0,k-1):
+	run = run+wts[i]
+
+for i in range(0,k-1):
+	wts[i] = (wts[i]/run)*0.9
+
+print(wts)
+print(len(PDF))
+print(PDF)
+
+def comppdf(x):
+	pdf = 0
+	for i in range(0,k):
+		wt = wts[i]
+		pd = 1
+		for j in range(0,i+1):
+			fix = PDF[i][j]
+			print(Dec[i][j])
+			pd = pd*(fix.pdf(submean(x,Dec[i][j])))
+			print(pd)
+		pdf = pdf + pd*wt
+		print(pdf)
+	return pdf
+
+samples = np.random.multivariate_normal(mean,estcov2,1000)
+
+pdf1 = np.zeros(1000)
+pdf2 = np.zeros(1000)
+
+for i in range(0,1000):
+	pdf1[i] = ref.pdf(samples[i])
+	pdf2[i] = comppdf(samples[i])
+
+plt.plot(pdf1)
+plt.plot(pdf2)
+plt.show()
 
 
 
